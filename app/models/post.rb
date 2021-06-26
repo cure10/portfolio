19dc8a760514ed4bae71800747a7e9267f3c5468
cure10 <soccercure112@gmail.com>
2021-06-26 11:02:23 +0000

    [fix]post.rb rateバリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -58,10 +58,6 @@ end
         notification.save if notification.valid?
     end
 
-validates :rate, numericality: {
-    less_than_or_equal_to: 5,
-    greater_than_or_equal_to: 1}, presence: true
-
 
 
 
